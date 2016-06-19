e9161f04e709b84595bd2464309fe4e977213f9b
Michael Gubbels <mrgubbels@gmail.com>
2016-06-18 18:52:15 -0700

    wget to Tumblr.

---
+++ Webcam_Frame_Downloader.py
@@ -0,0 +1,9 @@
+import pytumblr
+
+import os
+
+segment_count = 365
+#for i in range(0, segment_count):
+
+filename = './Temporary/out.png'
+os.system('wget wget http://82.244.209.27:5000/oneshotimage1?1466300729 -O %s' % filename)
